609a0967fc4300cdce5f82bdc9ebb404aad9b348
PRASATH978 <163582130+PRASATH978@users.noreply.github.com>
2025-04-22 09:30:44 +0530

    login page

--- cardgame_project/game/urls.py
+++ cardgame_project/game/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
+    path('', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('dragon-tiger/', views.play_dragon_tiger, name='play_dragon_tiger'),
-    path('', views.dashboard, name='home'),
+   
     path('dashboard/', views.dashboard, name='dashboard'),
 ]
